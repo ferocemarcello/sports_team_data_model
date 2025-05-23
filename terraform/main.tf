0d71ef27edef2b18cf29626564776d3a79d68869
Marcello Feroce <ferocemarcello@gmail.com>
2025-05-23 18:56:06 +0200

    edit main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,13 @@
+terraform {
+  required_providers {
+    postgresql = {
+      source = "cyrilgdn/postgresql"
+      version = "1.25.0"
+    }
+  }
+}
+
 resource "postgresql_database" "spond_analytics" {
   name  = "spond_analytics"
   owner = "postgres"
 }
-# Removed the 'null_resource.create_tables' block
-# as table creation is now handled by ingest_data.py
