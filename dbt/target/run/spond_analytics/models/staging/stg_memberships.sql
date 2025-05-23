cb3a3c4475004f066a04890722724b0a8b0ad6db
Marcello Feroce <ferocemarcello@gmail.com>
2025-05-23 20:21:20 +0200

    edit schema.yml

--- dbt/target/run/spond_analytics/models/staging/stg_memberships.sql
+++ dbt/target/run/spond_analytics/models/staging/stg_memberships.sql
@@ -4,11 +4,11 @@
     
   as (
     SELECT
-    memberships.id AS membership_id,
-    memberships.team_id,
+    memberships.membership_id AS membership_id, -- CORRECTED: changed to memberships.membership_id
+    memberships.team_id, -- This was already correct in previous instructions
     memberships.role_title,
     memberships.joined_at
 FROM "spond_analytics"."public"."raw_memberships" AS memberships
-INNER JOIN "spond_analytics"."public"."raw_teams" AS teams -- Ensures team_id exists in raw_teams
-  ON memberships.team_id = teams.team_id -- CORRECTED: changed to teams.team_id
+INNER JOIN "spond_analytics"."public"."raw_teams" AS teams
+  ON memberships.team_id = teams.team_id
   );
