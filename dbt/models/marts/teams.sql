b31ce0680bafdbdad444b3f34cbbdf855c9589ec
Marcello Feroce <ferocemarcello@gmail.com>
2025-05-23 20:01:39 +0200

    edit teams.sql

--- dbt/models/marts/teams.sql
+++ dbt/models/marts/teams.sql
@@ -1,3 +1,8 @@
+{{ config(
+    materialized='table',  -- Materialize as a table (instead of default view)
+    schema='public'        -- Explicitly set the schema for this model
+)}}
+
 SELECT
     team_id,
     team_name,
