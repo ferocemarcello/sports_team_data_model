0388c6a86b5981185decbb8326aeaf7093984d33
Marcello Feroce <ferocemarcello@gmail.com>
2025-05-23 22:29:47 +0200

    edit stg_event_rsvps

--- dbt/models/staging/stg_event_rsvps.sql
+++ dbt/models/staging/stg_event_rsvps.sql
@@ -5,5 +5,9 @@ SELECT
     membership_id,
     rsvp_status,
     responded_at
-FROM {{ source('public', 'raw_event_rsvps') }}
+FROM {{ source('public', 'raw_event_rsvps') }} AS event_rsvps
+INNER JOIN {{ ref('stg_events') }} AS events
+    ON event_rsvps.event_id = events.event_id
+INNER JOIN {{ ref('stg_memberships') }} AS memberships
+    ON event_rsvps.membership_id = memberships.membership_id
 WHERE rsvp_status IN ('0', '1', '2')
