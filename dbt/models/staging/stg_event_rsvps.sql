998acc36351fee479f1178fe51557746fc2a7963
Marcello Feroce <ferocemarcello@gmail.com>
2025-05-23 22:32:08 +0200

    edit stg_event_rsvps

--- dbt/models/staging/stg_event_rsvps.sql
+++ dbt/models/staging/stg_event_rsvps.sql
@@ -1,10 +1,10 @@
 -- dbt/models/staging/stg_event_rsvps.sql
 SELECT
-    event_rsvp_id,
-    event_id,
-    membership_id,
-    rsvp_status,
-    responded_at
+    event_rsvps.event_rsvp_id,
+    event_rsvps.event_id,
+    event_rsvps.membership_id,
+    event_rsvps.rsvp_status,
+    event_rsvps.responded_at
 FROM {{ source('public', 'raw_event_rsvps') }} AS event_rsvps
 INNER JOIN {{ ref('stg_events') }} AS events
     ON event_rsvps.event_id = events.event_id
--- dbt/target/run/spond_analytics/models/staging/stg_event_rsvps.sql
+++ dbt/target/run/spond_analytics/models/staging/stg_event_rsvps.sql
@@ -15,7 +15,11 @@ SELECT
     membership_id,
     rsvp_status,
     responded_at
-FROM "spond_analytics"."public"."event_rsvps"
+FROM "spond_analytics"."public"."event_rsvps" AS event_rsvps
+INNER JOIN "spond_analytics"."public"."stg_events" AS events
+    ON event_rsvps.event_id = events.event_id
+INNER JOIN "spond_analytics"."public"."stg_memberships" AS memberships
+    ON event_rsvps.membership_id = memberships.membership_id
 WHERE rsvp_status IN ('0', '1', '2')
   );
   
