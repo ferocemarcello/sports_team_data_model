797bc22168e6d723292b88f0a692f106c595d462
Marcello Feroce <ferocemarcello@gmail.com>
2025-05-23 21:32:15 +0200

    edit schema.yml

--- dbt/target/run/spond_analytics/models/staging/stg_event_rsvps.sql
+++ dbt/target/run/spond_analytics/models/staging/stg_event_rsvps.sql
@@ -10,12 +10,12 @@
   (
     -- dbt/models/staging/stg_event_rsvps.sql
 SELECT
-    rsvp_id,         -- Selects the column as it is named in the raw table
+    event_rsvp_id,
     event_id,
     membership_id,
-    status,          -- Selects the column as it is named in the raw table
-    rsvp_time        -- Selects the column as it is named in the raw table
-FROM "spond_analytics"."public"."event_rsvps"
-WHERE status IN ('accepted', 'declined', 'pending') -- Corrected filter to use 'status'
+    rsvp_status AS status,
+    responded_at AS rsvp_time
+FROM "spond_analytics"."public"."event_rsvps" -- <--- CHANGE THIS LINE
+WHERE rsvp_status IN ('accepted', 'declined', 'pending')
   );
   
