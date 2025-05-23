d7e718f9db0495eb0dcf3ebfc974137cf443a812
Marcello Feroce <ferocemarcello@gmail.com>
2025-05-23 20:23:51 +0200

    event_with_rsvp.sql

---
+++ dbt/target/compiled/spond_analytics/models/staging/schema.yml/not_null_stg_event_rsvps_membership_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select membership_id
+from "spond_analytics"."public_public"."stg_event_rsvps"
+where membership_id is null
+
+
---
+++ dbt/target/run/spond_analytics/models/staging/schema.yml/not_null_stg_event_rsvps_membership_id.sql
@@ -0,0 +1,24 @@
+
+    select
+      count(*) as failures,
+      count(*) != 0 as should_warn,
+      count(*) != 0 as should_error
+    from (
+      
+    
+  
+    
+    
+
+
+
+select membership_id
+from "spond_analytics"."public_public"."stg_event_rsvps"
+where membership_id is null
+
+
+
+  
+  
+      
+    ) dbt_internal_test
