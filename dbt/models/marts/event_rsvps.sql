5a995b19080124f3375817c9b892adddf8fa9e70
Marcello Feroce <ferocemarcello@gmail.com>
2025-05-23 22:55:57 +0200

    remove marts

--- dbt/models/marts/event_rsvps.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='table') }}
-
-SELECT
-    event_rsvp_id,
-    event_id,
-    membership_id,
-    rsvp_status,
-    responded_at -- From stg_event_rsvps where responded_at AS rsvp_time
-FROM {{ ref('event_rsvps') }}
--- dbt/models/marts/memberships.sql
+++
@@ -1,8 +0,0 @@
-{{ config(materialized='table') }}
-
-SELECT
-    membership_id
-    team_id,
-    role_title,
-    joined_at
-FROM {{ ref('memberships') }}
