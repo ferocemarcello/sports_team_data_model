7df701307d88a2eacedf51eeb8849de12651e32b
Marcello Feroce <ferocemarcello@gmail.com>
2025-05-23 21:23:38 +0200

    edit stg_event_rsvps.sql

--- dbt/models/staging/stg_event_rsvps.sql
+++ dbt/models/staging/stg_event_rsvps.sql
@@ -1,9 +1,9 @@
 -- dbt/models/staging/stg_event_rsvps.sql
 SELECT
-    rsvp_id,         -- Selects the column as it is named in the raw table
+    event_rsvp_id,
     event_id,
     membership_id,
-    status,          -- Selects the column as it is named in the raw table
-    rsvp_time        -- Selects the column as it is named in the raw table
-FROM {{ source('public', 'raw_event_rsvps') }}
-WHERE status IN ('accepted', 'declined', 'pending') -- Corrected filter to use 'status'
+    rsvp_status,   -- Keep the original raw column name
+    responded_at   -- Keep the original raw column name
+FROM {{ source('public', 'event_rsvps') }}
+WHERE rsvp_status IN ('accepted', 'declined', 'pending')
