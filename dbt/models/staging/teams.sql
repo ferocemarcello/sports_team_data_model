2b5a240fe3a7a670ca34541fc0d9a3476f5bb9a0
Marcello Feroce <ferocemarcello@gmail.com>
2025-05-23 23:35:08 +0200

    use dbt seed

--- dbt/models/staging/teams.sql
+++ dbt/models/staging/teams.sql
@@ -6,4 +6,4 @@ SELECT
 FROM
     {{ source('public', 'raw_teams') }} as teams
 INNER JOIN {{ ref('country_codes') }} AS country_codes
-    ON teams.country_code = country_codes."alpha-3"
+    ON teams.country_code = country_codes.alpha_three
