686d1204d4d06d0ffda1f2ebb9357faa4180c3a5
Marcello Feroce <ferocemarcello@gmail.com>
2025-05-24 15:50:28 +0200

    work on marts

--- dbt/models/marts/daily_active_teams.sql
+++ dbt/models/marts/daily_active_teams.sql
@@ -1,11 +1,28 @@
--- models/marts/daily_active_teams.sql (or wherever this view is defined)
+-- models/marts/daily_active_teams.sql
+-- How many distinct teams hosted or had events created each day?
 
+WITH team_event_activities AS (
+    -- Activity based on event start date
+    SELECT
+        team_id,
+        (TO_TIMESTAMP(event_start / 1000))::DATE AS activity_date
+    FROM {{ ref('stg_events') }}
+    WHERE event_start IS NOT NULL
+
+    UNION ALL
+
+    -- Activity based on event creation date
+    -- This captures "updated" if an event's creation is considered an update/activity
+    SELECT
+        team_id,
+        (TO_TIMESTAMP(created_at / 1000))::DATE AS activity_date
+    FROM {{ ref('stg_events') }}
+    WHERE created_at IS NOT NULL
+)
 SELECT
-    (TO_TIMESTAMP(se.event_start / 1000))::DATE AS event_date, -- <-- Fix is here
-    COUNT(DISTINCT sm.team_id) AS distinct_active_teams
-FROM {{ ref('stg_events') }} se
-JOIN {{ ref('stg_memberships') }} sm -- Assuming you join to get team_id
-  ON se.host_member_id = sm.membership_id -- Or whatever your join condition is
-WHERE se.event_start IS NOT NULL -- Exclude null timestamps
+    activity_date AS event_date,
+    COUNT(DISTINCT team_id) AS distinct_active_teams
+FROM team_event_activities
+WHERE activity_date IS NOT NULL -- Ensure the derived date is not null
 GROUP BY 1
 ORDER BY 1;
