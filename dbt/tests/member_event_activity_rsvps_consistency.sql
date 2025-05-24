de0a410b83c288fd28060e01cf2acb191ad6e463
Marcello Feroce <ferocemarcello@gmail.com>
2025-05-24 15:56:24 +0200

    work on tests

--- dbt/tests/member_event_activity_rsvps_consistency.sql
+++ dbt/tests/member_event_activity_rsvps_consistency.sql
@@ -2,11 +2,11 @@
 -- This test ensures that a member's accepted and declined RSVPs don't exceed their total RSVPs made,
 -- and all counts are non-negative.
 SELECT
-    member_id,
+    membership_id,
     total_rsvps_made,
     total_accepted_rsvps,
     total_declined_rsvps
-FROM {{ ref('rsvp_summary') }} -- Adjust 'member_event_activity' to your actual view name
+FROM {{ ref('rsvp_summary') }}
 WHERE
     total_rsvps_made < 0
     OR total_accepted_rsvps < 0
