777bdc1f7c282bb6744d14321dd64b294757c63a
Marcello Feroce <ferocemarcello@gmail.com>
2025-05-24 16:16:18 +0200

    delete test folder

--- dbt/tests/daily_event_summary_accepted_count_non_negative.sql
+++
@@ -1,8 +0,0 @@
--- tests/daily_event_summary_accepted_count_non_negative.sql
--- This test ensures that the count of accepted attendees is never negative.
-SELECT
-    event_id,
-    event_date,
-    total_accepted_attendees
-FROM {{ ref('rsvp_summary') }}
-WHERE total_accepted_attendees < 0
