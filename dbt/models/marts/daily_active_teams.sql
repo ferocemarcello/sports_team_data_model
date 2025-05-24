dc7715b3f1cc1a4de0d2b543bc82efb3672c75e9
Marcello Feroce <ferocemarcello@gmail.com>
2025-05-24 18:23:05 +0200

    work on marts

--- dbt/models/marts/daily_active_teams.sql
+++ dbt/models/marts/daily_active_teams.sql
@@ -5,7 +5,7 @@ WITH team_event_activities AS (
     -- Activity based on event start date
     SELECT
         team_id,
-        (TO_TIMESTAMP(event_start))::DATE AS activity_date -- REMOVED / 1000
+        (TO_TIMESTAMP(event_start))::DATE AS activity_date
     FROM {{ ref('stg_events') }}
     WHERE event_start IS NOT NULL
 
@@ -14,7 +14,7 @@ WITH team_event_activities AS (
     -- Activity based on event creation date
     SELECT
         team_id,
-        (TO_TIMESTAMP(created_at))::DATE AS activity_date -- REMOVED / 1000
+        (TO_TIMESTAMP(created_at))::DATE AS activity_date 
     FROM {{ ref('stg_events') }}
     WHERE created_at IS NOT NULL
 )
